8a7ee810430730ec9fb1a215e8efa9fcaad39d50
Michaelamakri <76743932+Michaelamakri@users.noreply.github.com>
2025-11-14 11:51:39 +0200

    Add files via upload

---
+++ add_restaurants.py
@@ -0,0 +1,19 @@
+from database_manager import DatabaseManager
+db=DatabaseManager()
+restaurants=[
+    ("Rest1","Athens","Greece"),
+    ("Rest2","Athens","Greece"),
+    ("Rest3","Thessaloniki","Greece"),
+    ("Rest4","Thessaloniki","Greece"),
+    ("Rest5","Patras","Greece"),
+    ("Rest6","Patras","Greece"),
+    ("Rest7","Chania","Greece"),
+    ("Rest8","Chania","Greece"),
+]
+for r in restaurants:
+    db.execute("""
+        INSERT INTO restaurants (name, city, country)
+        VALUES (?, ?, ?)
+    """, r)
+
+print("Restaurants inserted successfully!")
