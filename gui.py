720e4aa82143133e2fa5de9c838ad3197231ae8c
Michaelamakri <76743932+Michaelamakri@users.noreply.github.com>
2025-11-27 19:33:14 +0200

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+
 import tkinter as tk
 from tkinter import messagebox
 from database_manager import DatabaseManager
@@ -15,6 +16,7 @@ class gui(tk.Frame):
         tk.Label(self.root, text="Welcome to FoodApp",bg="#74AF74", font=("Arial", 22, "bold")).pack(pady=10) 
         tk.Button(self.root, text="Log In", width=20, height=2, command=self.login_window).pack(pady=10)
         tk.Button(self.root, text="Sign Up", width=20, height=2, command=self.signup_window).pack(pady=10)
+   
 
     def clear_window(self):
         for widget in self.root.winfo_children():
@@ -22,10 +24,8 @@ class gui(tk.Frame):
    
     
     def signup_window(self):
-        signup_win = tk.Toplevel(self.root)
-        signup_win.title("Sign Up")
-        signup_win.geometry("400x500")
-        signup_win.configure(bg="#74AF74")
+        self.clear_window()
+        self.root.title("Sign Up")
 
         labels = [
             "Name", "Email", "Phone", "Password", "Age",
@@ -34,8 +34,8 @@ class gui(tk.Frame):
         entries = {}
 
         for label in labels:
-            tk.Label(signup_win, text=f"{label}:").pack()
-            entry = tk.Entry(signup_win, show="*" if label =="Password" else None)
+            tk.Label(self.root, text=f"{label}:").pack()
+            entry = tk.Entry(self.root, show="*" if label =="Password" else None)
             entry.pack()
             entries[label] = entry
 
@@ -57,24 +57,22 @@ class gui(tk.Frame):
                     data["Age"], data["Country"], data["City"], data["Region"]
                 ))
                 messagebox.showinfo("Success", "Account created successfully!")
-                signup_win.destroy()
+                
                 
                 
             except Exception as e:
                 messagebox.showerror("Database Error ","{e}")
 
-        tk.Button(signup_win, text="Sign Up", command=signup_action).pack(pady=20)
+        tk.Button(self.root, text="Sign Up", command=signup_action).pack(pady=20)
      
     def login_window(self):
-        login_win = tk.Toplevel(self.root)
-        login_win.title("Log In")
-        login_win.geometry("400x500")
-        login_win.configure(bg="#74AF74")
-        tk.Label(login_win, text="Email:").pack()
-        email_entry = tk.Entry(login_win)
+        self.clear_window()
+        self.root.title("Log In")
+        tk.Label(self.root, text="Email:").pack()
+        email_entry = tk.Entry(self.root)
         email_entry.pack()
-        tk.Label(login_win, text="Password:").pack()
-        pass_entry = tk.Entry(login_win, show="*")
+        tk.Label(self.root, text="Password:").pack()
+        pass_entry = tk.Entry(self.root, show="*")
         pass_entry.pack()
         def login_action(event=None):  
             email = email_entry.get().strip()
@@ -90,14 +88,14 @@ class gui(tk.Frame):
                 name = user[0][1]
                 city = user[0][7] 
                 messagebox.showinfo("Welcome", f"Welcome back, {name}!")
-                login_win.destroy()
+                
                 self.clear_window()
                 self.show_restaurants_screen(name,city)
             else:
                 messagebox.showerror("Error", "Invalid email or password")
                 
-        tk.Button(login_win, text="Log In", command=login_action).pack(pady=20)
-        login_win.bind("<Return>", login_action)
+        tk.Button(self.root, text="Log In", command=login_action).pack(pady=20)
+        self.root.bind("<Return>", login_action)
         email_entry.focus_set()
     
     def show_restaurants_screen(self, username,usercity):
@@ -117,13 +115,18 @@ class gui(tk.Frame):
         messagebox.showinfo("Cart", f"Added {name} to cart!")
     def open_menu(self, restaurant_name):
         self.clear_window()
+        account_btn = tk.Button(self.root, text="👤",font=("Arial", 18),bg="#74AF74",relief="flat",
+        command=lambda: self.open_profile_sidebar("User")   # ή πέρασε username αν το κρατάς κάπου
+    )
+        account_btn.place(relx=0.95, rely=0.02, anchor="ne")
         tk.Label(self.root,text=f"Menu of {restaurant_name}",font=("Arial", 22, "bold"),bg="#74AF74").pack(pady=10)
 
         menu_items = self.db.fetchall("""
-            SELECT item_name, price 
-            FROM menu 
-            WHERE rest_id = (SELECT rest_id FROM restaurants WHERE name=?)
-        """, (restaurant_name,))
+        SELECT item_name, price FROM menu
+        WHERE rest_id = (
+            SELECT rest_id FROM restaurants WHERE name = ?
+        )
+    """, (restaurant_name,))
 
         if menu_items==[]:
             tk.Label(self.root, text="No items found.", bg="#74AF74").pack(pady=10)
@@ -133,12 +136,25 @@ class gui(tk.Frame):
             frame.pack(pady=5)
             tk.Label(frame, text=f"{item_name} price:{price}€", bg="#74AF74").pack(side="left", padx=10)
             tk.Button(frame, text="Add to Cart",command=lambda n=item_name, p=price: self.add_to_cart(n, p)).pack(side="right")
+    def open_profile_sidebar(self, username):
+        
+        self.sidebar = tk.Frame(self.root, bg="white", width=250, height=self.root.winfo_height())
+        self.sidebar.place(relx=1.0, y=0, anchor="ne")
+        tk.Label(self.sidebar, text=username, font=("Arial", 18, "bold"), bg="white").pack(pady=20)
+        
+        options = [
+        ("Εισερχόμενα μηνύματα", self.fake_page),
+        ("Ο λογαριασμός μου", self.fake_page),
+        ("Παραγγελίες", self.fake_page),
+        ("Διευθύνσεις", self.fake_page),
+        ]
+        for text, action in options:
+            btn = tk.Button(self.sidebar, text=text, anchor="w",relief="flat", bg="white", font=("Arial", 12),command=action
+        )
+            btn.pack(fill="x", pady=5, padx=15)
+            tk.Button(self.sidebar, text="Κλείσιμο", bg="#FF5555", fg="white",command=self.sidebar.destroy).pack(pady=20)
+    def fake_page(self):
+        messagebox.showinfo("Info", "Αυτή η σελίδα δεν είναι έτοιμη ακόμη.")
 
 
-
-    
-    
-    
-
-
-
+   
